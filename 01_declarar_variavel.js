88db4bfb4c03e6e9d62756a45f3ae752437d0ac4
tayroneMachado <tatank_@live.com>
2025-04-28 22:41:45 -0300

    exercicio 1

---
+++ 01_declarar_variavel.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+//Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima no console
+let idade = 30
+let peso = 75
+console.log(idade)
+console.log(peso)
