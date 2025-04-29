ceabd0e8f8f05b8263100d67bc7f58034822eda8
tayroneMachado <tatank_@live.com>
2025-04-28 22:53:59 -0300

    exercicio 03

---
+++ 03_numero_maximo.js
@@ -0,0 +1,3 @@
+//Declare uma variável chamada "númeroMáximo", atribua a ela o valor máximo que um número pode ter  em javascript e imprima no console.
+let numeroMaximo = Number.MAX_VALUE
+console.log(numeroMaximo)
